eb12feb38b798119f9311e9e560997131bb7f6f4
Porawit Poboonma <ball6847@gmail.com>
2017-04-06 00:03:32 +0700

    scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SnippetFormComponent } from './components/snippet-form/snippet-form.component';
+import { SnippetListComponent } from './components/snippet-list/snippet-list.component';
+import { SnippetListItemComponent } from './components/snippet-list-item/snippet-list-item.component';
+
+const routes: Routes = [
+  { path: '', component: DashboardComponent, pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SnippetFormComponent,
+    SnippetListComponent,
+    SnippetListItemComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/snippet-list-item/snippet-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'snippet-list-item',
+  templateUrl: './snippet-list-item.component.html',
+  styleUrls: ['./snippet-list-item.component.css']
+})
+export class SnippetListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
